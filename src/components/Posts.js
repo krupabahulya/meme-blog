ba94247ce920f93273a377ff7af9e20f4cc447a3
Shawndek <sdek@gmx.de>
2022-03-23 11:44:42 +0100

    layout in home

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,10 +3,11 @@ import Post from "./Post";
 
 const Posts = ({ posts }) => {
   return (
-    <div>
+    <div className="d-flex p-2 flex-row justify-content-around flex-wrap">
       {posts.map((article, index) => (
         <Post article={article} key={index} />
-      ))}
+       ))} <div className="p-2">
+        </div>
     </div>
   );
 };
--- src/components/Sidenavbar.js
+++ src/components/Sidenavbar.js
@@ -1,38 +1,31 @@
 import React from "react";
 
-const Sidenavbar = ({ article }) => {
-  console.log(article);
-  const { categories } = article.fields;
+const Sidenavbar = () => {
+
   return (
         <div>
-             <h4 >
-               <a href="#">Meme <span>Categories</span></a>
-             </h4>
-               <div className="nav-wrap">
-{/*                   <nav className="main-nav" role="navigation">
-                    <ul className="unstyled list-hover-slide">
-                     <li><a href="#">latest</a></li>
-                     <li><a href="#">classics</a></li>
-                     <li><a href="#">animals</a></li>
-                     <li><a href="#">crypto</a></li>
-                     <li><a href="#">cooking</a></li>
-                     <li><a href="#">relationship</a></li>
-                    </ul>
-<<<<<<< HEAD
-                  </nav> */}
+     <div className="form-group container">
+     <div className="d-flex p-2">
+     <div className="justify-content-start align-self-center">
+      <label for="exampleSelect1" className="form-label mt-4"><h4 >Meme Categories:</h4></label>
+      </div>
+      <div className="p-2">
+      </div>
+      <div className="align-self-center">
+      <select className="form-select" id="exampleSelect1">
+        <option>all</option>
+        <option>latest</option>
+        <option>classics</option>
+        <option>animals</option>
+        <option>crypto</option>
+        <option>cooking</option>
+        <option>relationship</option>
+      </select>
+      </div>
+    </div>
+    </div>
+ </div>
 
+   )};
 
-<div class="btn-group-vertical">
-  <button type="button" class="btn btn-primary" href="#">{[categories]}</button>
-</div>
-
-
-=======
-                  </nav>
-                 
->>>>>>> repo/main
-               </div>
-        </div>
-    )
-}
 export default Sidenavbar;

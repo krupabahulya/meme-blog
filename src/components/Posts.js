27e17614aeee1b5cde12adcc177c7f39fdcc8564
root <root@DESKTOP-GVQ0Q5H.localdomain>
2022-03-23 17:04:26 +0100

    lastest

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,8 +6,8 @@ const Posts = ({ posts }) => {
     <div className="d-flex p-2 flex-row justify-content-around flex-wrap">
       {posts.map((article, index) => (
         <Post article={article} key={index} />
-       ))} <div className="p-2">
-        </div>
+      ))}{" "}
+      <div className="p-2"></div>
     </div>
   );
 };

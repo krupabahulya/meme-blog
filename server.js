c1a9ccc17efa33fc69522d5157d6907c0bec7376
krupabahulya <krupabahulya.y@gmail.com>
2022-04-10 21:33:30 +0200

    see meme data on console

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+  });
 app.use('/memes', memesRouter);
 app.get('/', (req,res) => {
     res.send('Memes API')
